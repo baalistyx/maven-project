02dd78cf479bb8a4ec310982057785de2dec19d1
Richard Geist <rgeist@airbnb.com>
2018-06-08 21:27:50 -0700

    fixed.. what ever maven crap

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
   }
 
   /** .
-   * Javadoc
+   * Javadoc {@inheretDoc}
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
